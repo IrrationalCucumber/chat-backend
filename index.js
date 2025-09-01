2d6bb5c42631d1d48e5d0ae845ed7217e7741eda
adrean paul sorono <adreanpaulsorono@gmail.com>
2025-09-01 14:31:14 +0800

    Created endpoint for sample user data

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 var app = express();
+const users = require("./sample data/user");
+
 app.get("/", (_, res) => {
   res.send("Hello World!");
 });
-app.listen(10000, function () {
+app.listen(8800, function () {
   console.log("Started application on port %d", 8800);
+});
+
+// endpoint to get sample users data
+app.get("/users", (_, res) => {
+  res.send(users);
 });
