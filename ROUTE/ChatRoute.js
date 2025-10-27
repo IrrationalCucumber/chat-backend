40c318c95ba5abb1b7281a971e41a9dc9d7e00cd
adrean paul sorono <adreanpaulsorono@gmail.com>
2025-10-27 17:12:19 +0800

    Add route for setting favorite

--- ROUTE/ChatRoute.js
+++ ROUTE/ChatRoute.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.get("/convos", ChatController.fetchChat); // to retrieve chats
 router.get("/chat/:id", ChatController.fetchChatByID); // to retrieve chat of user
 router.get("/messages/:id", ChatController.fetchMessageByID); //messages
-router.get("/send", ChatController.addNewMessage); //ROUTE for INSERTING NEW MESSAFW
-//ROUTE for UPDATE CONVO to FAVORITE
+router.post("/send", ChatController.addNewMessage); //ROUTE for INSERTING NEW MESSAFW
+router.update("/update", ChatController.setConvoToFavorite); //ROUTE for UPDATE CONVO to FAVORITE
 module.exports = router;
